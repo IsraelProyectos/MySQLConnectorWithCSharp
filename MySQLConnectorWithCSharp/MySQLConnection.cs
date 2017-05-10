b507a5315797e4e4755602c3eef7ddd4e3bddef2
IsraelPuyol <israelpuyol@hotmail.com>
2017-05-10 19:48:00 +0200

    c

--- MySQLConnectorWithCSharp/MySQLConnection.cs
+++ MySQLConnectorWithCSharp/MySQLConnection.cs
@@ -22,9 +22,9 @@ namespace MySQLConnectorWithCSharp
                 MySqlConnection conn = new MySqlConnection(builder.ToString());
                 conn.Open();
                 MessageBox.Show("Conexion correcta");
-                MySqlCommand cmd = conn.CreateCommand();
-                cmd.CommandText = "INSERT INTO personaje (nombre, apellido1, apellido2) VALUES ('lokomotiv', 'Puyol', 'soriano')";
-                cmd.ExecuteNonQuery();
+                //MySqlCommand cmd = conn.CreateCommand();
+                //cmd.CommandText = "INSERT INTO personaje (nombre, apellido1, apellido2) VALUES ('lokomotiv', 'Puyol', 'soriano')";
+                //cmd.ExecuteNonQuery();
                 conn.Close();
             }
             catch(Exception e)
